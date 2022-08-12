674ee98019471a7252a01ae481d9fa604ce60d29
ageevDmitry <ageevdmitry2018@gmail.com>
2022-08-12 10:18:28 +0300

    Add login with token

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,6 +1,32 @@
 import Header from '../../components/header/header';
+import {useRef, FormEvent} from 'react';
+// import {useNavigate} from 'react-router-dom';
+import {useAppDispatch} from '../../hooks';
+import {loginAction} from '../../store/api-action';
+import {AuthData} from '../../types/auth-data';
+// import {AppRoute} from '../../const';
 
 function Login (): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  // const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
 
   return (
     <div className="page page--gray page--login">
@@ -9,10 +35,11 @@ function Login (): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={handleSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  ref={loginRef}
                   className="login__input form__input"
                   type="email"
                   name="email"
@@ -23,6 +50,7 @@ function Login (): JSX.Element {
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
                 <input
+                  ref={passwordRef}
                   className="login__input form__input"
                   type="password"
                   name="password"
