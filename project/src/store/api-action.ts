a7915a322dca00aad33b963ca0bef386747cf984
ageevDmitry <ageevdmitry2018@gmail.com>
2022-08-17 08:12:44 +0300

    Fix load UserData

--- project/src/store/api-action.ts
+++ project/src/store/api-action.ts
@@ -93,7 +93,8 @@ export const checkAuthAction = createAsyncThunk<void, undefined, {
     'user/checkAuth',
     async (_arg, {dispatch, extra: api}) => {
       try {
-        await api.get(APIRoute.Login);
+        const {data} = await api.get<UserData>(APIRoute.Login);
+        dispatch(loadUserData(data));
         dispatch(requireAuthorization(AuthorizationStatus.Auth));
       } catch {
         dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
